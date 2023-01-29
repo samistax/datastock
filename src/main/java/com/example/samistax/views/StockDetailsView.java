cffd48b3c2ae1637c11226230035cdd87561d804
Marcus Hellberg <marcus@vaadin.com>
2023-01-29 08:35:54 -0800

    Remove redundant column from grid

--- src/main/java/com/example/samistax/views/StockDetailsView.java
+++ src/main/java/com/example/samistax/views/StockDetailsView.java
@@ -3,7 +3,6 @@ package com.example.samistax.views;
 import com.example.samistax.astra.data.StockPrice;
 import com.example.samistax.astra.service.StockPriceService;
 import com.example.samistax.components.StockSymbolComboBox;
-import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Paragraph;
@@ -26,7 +25,6 @@ public class StockDetailsView extends VerticalLayout {
         var stockPriceGrid = new Grid<StockPrice>();
         var stockSymbolSelector = new StockSymbolComboBox("Stock Ticker");
 
-        stockPriceGrid.addColumn(StockPrice::getSymbol).setHeader("Symbol");
         stockPriceGrid.addColumn(StockPrice::getTime).setAutoWidth(true).setHeader("Time").setSortable(true);
         stockPriceGrid.addColumn(stockPrice -> decimalFormat.format(stockPrice.getOpen())).setHeader("Open").setSortable(true);
         stockPriceGrid.addColumn(stockPrice -> decimalFormat.format(stockPrice.getHigh())).setHeader("High").setSortable(true);
@@ -34,7 +32,7 @@ public class StockDetailsView extends VerticalLayout {
         stockPriceGrid.addColumn(stockPrice -> decimalFormat.format(stockPrice.getClose())).setHeader("Close").setSortable(true);
         stockPriceGrid.addColumn(StockPrice::getVolume).setHeader("Volume").setSortable(true);
 
-        stockSymbolSelector.setWidth(50, Unit.PERCENTAGE);
+        stockSymbolSelector.setWidth("50%");
         stockSymbolSelector.addValueChangeListener(e -> {
             var ticker = e.getValue().symbol();
 
